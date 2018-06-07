e37179c5a1be4bcfedef3ddcae4577993b163df9
Chris Gorman <chrisjohgorman@gmail.com>
2018-06-07 19:49:46 -0400

    rewrote ISR routine to deal with leap years

--- clock.c
+++ clock.c
@@ -20,13 +20,28 @@ Hardware: HD44780 compatible LCD text display
 void wait_until_key_pressed(void);
 void lcd_update_clock(void);
 void lcd_update_date(void);
+char day_of_week(int, char, char);
+char day_of_month(int, char, char, char);
+char leap_year(int);
 
 /*
  * Add hour minute second
  */
-volatile unsigned char hours = 0;
-volatile unsigned char minutes = 16;
-volatile unsigned char seconds = 50;
+unsigned int year = 2018;
+unsigned char month = 6;
+unsigned char day = 7;
+unsigned char hour = 0;
+unsigned char minute = 16;
+volatile unsigned char second = 50;
+volatile unsigned char lastdom;
+
+const char *weekdays[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+const char *months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+	"Aug", "Sep", "Oct", "Nov", "Dec" };
+const char daytab[2][12] = {
+        {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};  
 
 /*
  * Interrupt service routine
@@ -69,47 +84,145 @@ int main(void)
 
 void lcd_update_date()
 {
+	unsigned char weekday;
+	char buffer[7];
 	lcd_clrscr();
-        lcd_puts("Thu Jun 7, 2018\n");
+	weekday = day_of_week(year, month, day);
+	lcd_puts(weekdays[weekday]);
+	lcd_putc(' ');
+	lcd_puts(months[month]);
+	lcd_putc(' ');
+	itoa(day, buffer, 10);
+	lcd_puts(buffer);
+	lcd_putc(',');
+	lcd_putc(' ');
+        itoa(year, buffer, 10);
+	lcd_puts(buffer);
+	lcd_putc('\n');
 }
+
+/*
+ * https://hackaday.com/2012/07/16/automatic-daylight-savings-time-compensation-for-your-clock-projects/
+ */
+
+char day_of_week(int year, char month, char day)
+{
+       static char table[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+       year -= month < 3;
+       return (year + year/4 - year/100 + year/400 + table[month-1] + day) % 7;
+}
+
+char day_of_month(int year, char month, char dow, char week){
+  	char target_date = 1;
+  	char first_dow = day_of_week(year, month, target_date);
+  	while (first_dow != dow){
+    		first_dow = (first_dow+1)%7;
+    		target_date++;
+  	}
+
+  	target_date += (week-1)*7;
+  	return target_date;
+}
+
+char leap_year(int year) {
+	return (((year%4 == 0 && year%100 != 0) || year%400 ==0));
+}
+/*
+ * https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week
+ */
 
 void lcd_update_clock() 
 {
     	char buffer[7];
 
 	lcd_gotoxy(4,1);  
-        itoa(hours/10, buffer, 10);
+        itoa(hour/10, buffer, 10);
         lcd_puts(buffer);
-        itoa(hours%10 , buffer, 10);
+        itoa(hour%10 , buffer, 10);
         lcd_puts(buffer);
         lcd_putc(':');
-	itoa(minutes/19, buffer, 10);
+	itoa(minute/19, buffer, 10);
 	lcd_puts(buffer);
-	itoa(minutes%10, buffer, 10);
+	itoa(minute%10, buffer, 10);
 	lcd_puts(buffer);
 	lcd_putc(':');
-	itoa(seconds/10, buffer, 10);	
+	itoa(second/10, buffer, 10);	
 	lcd_puts(buffer);
-	itoa(seconds%10, buffer, 10);	
+	itoa(second%10, buffer, 10);	 
 	lcd_puts(buffer);
 }
+/*
+ISR(TIMER1_COMPA_vect)
+{
+        second++;
+
+        if(second >= 60)
+        {
+                second -= 60;
+                minute++;
+        }
+        if(minute >= 60)
+        {
+                minute = 60;
+                hour++;
+        }
+        if(hour > 23) {
+                hour -= 24;
+		day++;
+	}
+	if(((leap_year(year) && day == 29) && month == 2)) {
+		day = 1;
+		month++;
+	} else if (((!leap_year(year) && day == 28) && month == 2)) {
+		day = 1;
+		month++;
+	} else if (day == 30 && 
+		(month == 4 || month == 6 || month == 9 || month == 11)) {
+		day = 1;
+		month++;
+	} else if (day == 31) {
+		day = 1;
+		month++;
+	}
+	if(month > 12) {
+		month = 0;
+		year++;
+	}
+	lcd_update_date();
+	lcd_update_clock();
+} */
+
 
 ISR(TIMER1_COMPA_vect)
 {
-        seconds++;
+        second++;
 
-        if(seconds >= 60)
+        if(second > 59)
         {
-                seconds -= 60;
-                minutes++;
+                second = 0;
+                minute++;
+                if(minute > 59)
+                {
+                        minute = 0;
+                        hour++;
+                        if(hour > 23) {
+                                hour = 0;
+                                day++;
+                                if(!leap_year(year))
+                                        lastdom = daytab[0][month -1];
+                                else
+                                        lastdom = daytab[1][month -1];
+                                if(day > lastdom) {
+                                        day = 1;
+                                        month++;
+                                        if (month > 12) {
+                                                month = 1;
+                                                year++;
+                                        }
+                                }
+                        }
+                }
         }
-        if(minutes >= 60)
-        {
-                minutes = 60;
-                hours++;
-        }
-        if(hours > 23)
-                hours -= 24;
 	lcd_update_date();
 	lcd_update_clock();
 }
