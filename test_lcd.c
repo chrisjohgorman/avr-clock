9c8fd4520b4d7f898575edbe69019df8b463c42c
Chris Gorman <chrisjohgorman@gmail.com>
2018-06-06 20:11:11 -0400

    Removing coopyright character, we shouldn't need it.

--- test_lcd.c
+++ test_lcd.c
@@ -11,18 +11,8 @@ Hardware: HD44780 compatible LCD text display
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "lcd.h"
-
-
-/*
-** constant definitions
-*/
-static const PROGMEM unsigned char copyRightChar[] =
-{
-	0x07, 0x08, 0x13, 0x14, 0x14, 0x13, 0x08, 0x07,
-	0x00, 0x10, 0x08, 0x08, 0x08, 0x08, 0x10, 0x00
-};
-
 
 /*
 ** function prototypes
@@ -49,8 +39,6 @@ int main(void)
 {
     char buffer[7];
     int  num=134;
-    unsigned char i;
-    
     
     DDRD &=~ (1 << PD2);        /* Pin PD2 input              */
     PORTD |= (1 << PD2);        /* Pin PD2 pull-up enabled    */
@@ -140,16 +128,6 @@ int main(void)
        lcd_clrscr();   /* clear display home cursor */
        
        lcd_puts("Copyright: ");
-       
-       /*
-        * load two userdefined characters from program memory
-        * into LCD controller CG RAM location 0 and 1
-        */
-       lcd_command(_BV(LCD_CGRAM));  /* set CG RAM start address 0 */
-       for(i=0; i<16; i++)
-       {
-           lcd_data(pgm_read_byte_near(&copyRightChar[i]));
-       }
        
        /* move cursor to position 0 on line 2 */
        /* Note: this switched back to DD RAM adresses */
