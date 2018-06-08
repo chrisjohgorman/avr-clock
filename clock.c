2aa99ce6e13238d2831854a5f494e7b0dcec861f
Chris Gorman <chrisjohgorman@gmail.com>
2018-06-08 12:18:35 -0400

    Added license and author information for day_of_month

--- clock.c
+++ clock.c
@@ -104,16 +104,39 @@ void lcd_update_date()
 	lcd_putc('\n');
 }
 
-/*
- * https://hackaday.com/2012/07/16/automatic-daylight-savings-time-compensation-for-your-clock-projects/
- */
-
 char day_of_week(int year, char month, char day)
 {
        static char table[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
        year -= month < 3;
        return (year + year/4 - year/100 + year/400 + table[month-1] + day) % 7;
 }
+
+/* The following function used to be called NthDate and was authored by
+Mike Szczys.  It can be found at GitHub and has the following License.
+
+https://github.com/szczys/Automatic-Daylight-Savings-Time
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute 
+this software, either in source code form or as a compiled binary, for any 
+purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of 
+this software dedicate any and all copyright interest in the software to the 
+public domain. We make this dedication for the benefit of the public at large 
+and to the detriment of our heirs and successors. We intend this dedication 
+to be an overt act of relinquishment in perpetuity of all present and future 
+rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   For more information, please refer to https://unlicense.org  */
 
 char day_of_month(int year, char month, char dow, char week){
   	char target_date = 1;
