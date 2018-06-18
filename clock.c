9e4c0e9301786e1274bf1024c8d361d06e5f8530
Chris Gorman <chrisjohgorman@gmail.com>
2018-06-18 14:08:34 -0400

    Modified comments to be uniformly c++ style
    Removed ISR for switch use as is not correct
    Removed GIFR and GICR as they are not correct
    
    Made PORTD use PD0, PD1 and PD2 instead of PD2, PD3 and PD4 as we
    are using PD4/TOSC1/XCK0 for our 32768Hz clock

--- clock.c
+++ clock.c
@@ -1,8 +1,8 @@
-/*************************************************************************
-Title:    Clock 
-Author:   Chris Gorman <chrisjohgorman@gmail.com>
-File:     clock.c
- **************************************************************************/
+// Title:    Clock 
+// Author:   Chris Gorman <chrisjohgorman@gmail.com>
+// File:     clock.c
+//
+
 #include <stdlib.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
@@ -10,9 +10,9 @@ File:     clock.c
 #include <avr/interrupt.h>
 #include "lcd.h"
 
-/*
- ** function prototypes
- */
+//
+// function prototypes
+// 
 void lcd_display_clock(void);
 void lcd_display_second(void);
 void lcd_display_minute(void);
@@ -33,9 +33,9 @@ char day_of_week(int, char, char);
 char day_of_month(int, char, char, char);
 char leap_year(int);
 
-/*
- * Add hour minute second
- */
+//
+// Add hour minute second
+//
 volatile unsigned int year = 2018;
 volatile unsigned char month = 6;
 volatile unsigned char day = 11;
@@ -54,22 +54,16 @@ const char daytab[2][12] = {
 	{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };  
 
-/*
- * Interrupt service routine
- */
-ISR(INT0_vect);
+//
+// Interrupt service routine
+//
 
 ISR(TIMER1_COMPA_vect);
 
 int main(void)
 {
-	DDRD &=~ (1 << PD2) | (1 << PD3) | (1 << PD4);  /* Pin PD2, PD3, PD4 input */
-	PORTD |= (1 << PD2);        /* Pin PD2 pull-up enabled    */
-	PORTD |= (1 << PD3);        /* Pin PD3 pull-up enabled    */
-	PORTD |= (1 << PD4);        /* Pin PD4 pull-up enabled    */
-
-	GIFR = (1 << INTF0);
-	GICR = (1 << INT0);
+	DDRD &=~ (1 << PD0) | (1 << PD1) | (1 << PD2); // PD0, PD1, PD2 input
+	PORTD = (1 << PD0) | (1 << PD1) | (1 << PD2); // pullup resistors 
 
 	TCCR1B |= (1 << WGM12);
 	TIMSK |= (1 << OCIE1A);
@@ -77,10 +71,10 @@ int main(void)
 	//OCR1A = 32768 - 1; // set CTC compare value to 1Hz at 32.768KHz 
         		     //	with a prescaler of 1 (hex 0x7FFF) 
 	OCR1A = 16525-1;
-	//TCCR1B |= (1 << CS10); /* set to clk_io/1 (No prescaling) */
+	//TCCR1B |= (1 << CS10); // set to clk_io/1 (No prescaling) 
 	TCCR1B |= ((1 << CS10) | (1 << CS11));
 
-	/* initialize display, cursor off */
+	// initialize display, cursor off
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
 
@@ -89,19 +83,21 @@ int main(void)
 		switch (set_time % 7)
 		{
 		case 0:
-			// if button press up, year++
-				//if year == 10000,
-				// year = 0;
 			set_year();
+			// if button press up
+			// 	year++
+			//	if year == 10000
+			// 		year = 0
 			if(bit_is_clear(PIND, PD3))
 			{
 				year++;
 				if (year == 10000)
 					year = 0;
 			}
-			// if button press down, year--;
-				//if year == -1,
-				// year == 9999;
+			// if button press down
+			//  	year--
+			//	if year == -1
+			// 		year == 9999
 			if(bit_is_clear(PIND, PD4))
 			{
 				year--;
@@ -110,82 +106,88 @@ int main(void)
 			}
 			break;
 		case 1:
-
 			set_month();
+			// if button press up
+			// 	month++
+			//	if month == 13
+			//		month = 1
 			if(bit_is_clear(PIND, PD3))
 			{
 				month++;
 				if (month == 13)
 					month = 1;
 			}
-
+			// if button press down
+			//	month--
+			//	if month == 0
+			//		month = 12
 			if(bit_is_clear(PIND, PD4))
 			{
 				month--;
 				if (month == 0)
 					month = 12;
 			}
-
 			break;
 		case 2:
-			// if button press up day++;
-				// if day > lastdom
-				// day = 1;
-			// if button press down day--;
-				// if day < 1
-				// day = 1;
 			set_day();
-
+			// FIXME lastdom is not set until the month is set once
+			// so call it here in case someone wants to go
+			// backwards through the months, otherwise we get 
+			// month = 255, 254, etc.
+			//
+			// if button press up
+			// 	day++
+			// 	if day > lastdom
+			// 		day = 1
 			if(!leap_year(year))
 				lastdom = daytab[0][month -1];
 			else
 				lastdom = daytab[1][month -1];
-
 			if(bit_is_clear(PIND, PD3))
 			{
 				day++;
 				if (day > lastdom)
 					day = 1;
 			}
-
+			// if button press down 
+			// 	day--
+			// 	if day < 1
+			// 		day = lastdom
 			if(bit_is_clear(PIND, PD4))
 			{
 				day--;
 				if (day < 1)
 					day = lastdom;
 			}
-
 			break;
 		case 3:
-			// if button press up hour++;
-				// if hour == 24
-				// hour = 0;
-			// if button press down hour--;
-				// if hour == -1
-				// hour = 23;
 			set_hour();
+			// if button press up 
+			// 	hour++
+			// 	if hour == 24
+			// 		hour = 0
 			if(bit_is_clear(PIND, PD3))
 			{
 				hour++;
 				if (hour > 23)
 					hour = 0;
 			}
-
+			// if button press down 
+			// 	hour--
+			// 	if hour == -1
+			// 		hour = 23
 			if(bit_is_clear(PIND, PD4))
 			{
 				hour--;
 				if (hour < 0)
 					hour = 23;
 			}
-
 			break;
 		case 4:
-			// if button press up minute++;
-				// if minute == 60
-				// minute = 1;
-			// if button press down minute--;
-				// if minute == -1
-				// minute = 59
+			// if button press up 
+			// 	minute++
+			// 	if minute == 60
+			// 		minute = 1
 			set_minute();
 			if(bit_is_clear(PIND, PD3))
 			{
@@ -193,28 +195,29 @@ int main(void)
 				if (minute == 60)
 					minute = 1;
 			}
-
+			// if button press down 
+			// 	minute--
+			// 	if minute == -1
+			//		minute = 59
 			if(bit_is_clear(PIND, PD4))
 			{
 				minute--;
 				if (minute < 0)
 					minute = 59;
 			}
-
 			break;
 		case 5:
 			set_second();
+			// if button press up second = 0;
 			if(bit_is_clear(PIND, PD3))
 			{
 				second = 0;
 			}
-
+			// if button press down second = 0;
 			if(bit_is_clear(PIND, PD4))
 			{
 				second = 0;
 			}
-			// if button press up second = 0;
-			// if button press down second = 0;
 			break;
 		case 6:
 			break;
@@ -343,32 +346,32 @@ char day_of_week(int year, char month, char day)
 	return (year + year/4 - year/100 + year/400 + table[month-1] + day) % 7;
 }
 
-/* The following function used to be called NthDate and was authored by
-Mike Szczys.  It can be found at GitHub and has the following License.
-
-https://github.com/szczys/Automatic-Daylight-Savings-Time
-
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or distribute 
-this software, either in source code form or as a compiled binary, for any 
-purpose, commercial or non-commercial, and by any means.
-
-In jurisdictions that recognize copyright laws, the author or authors of 
-this software dedicate any and all copyright interest in the software to the 
-public domain. We make this dedication for the benefit of the public at large 
-and to the detriment of our heirs and successors. We intend this dedication 
-to be an overt act of relinquishment in perpetuity of all present and future 
-rights to this software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
-ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-   For more information, please refer to https://unlicense.org  */
+// The following function used to be called NthDate and was authored by
+// Mike Szczys.  It can be found at GitHub and has the following License.
+//
+// https://github.com/szczys/Automatic-Daylight-Savings-Time
+//
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or distribute 
+// this software, either in source code form or as a compiled binary, for any 
+// purpose, commercial or non-commercial, and by any means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors of 
+// this software dedicate any and all copyright interest in the software to the 
+// public domain. We make this dedication for the benefit of the public at large
+// and to the detriment of our heirs and successors. We intend this dedication 
+// to be an overt act of relinquishment in perpetuity of all present and future 
+// rights to this software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//   For more information, please refer to https://unlicense.org 
 
 char day_of_month(int year, char month, char dow, char week){
 	char target_date = 1;
@@ -382,22 +385,20 @@ char day_of_month(int year, char month, char dow, char week){
 	return target_date;
 }
 
+// return the day of the first Sunday in November
 char fall_savings(void) {
-	/* return the day of the first sunday in november */
 	return (day_of_month(year,month,day_of_week(year,month,day), 0));
 }
 
+// return the day of the second Sunday in March
 char spring_savings(void) {
-	/* return the day of the second sunday in march */
 	return (day_of_month(year,month,day_of_week(year,month,day), 1));
 }
 
+// https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week
 char leap_year(int year) {
 	return (((year%4 == 0 && year%100 != 0) || year%400 ==0));
 }
-/*
- * https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week
- */
 
 void lcd_display_clock() 
 {
@@ -429,11 +430,13 @@ ISR(TIMER1_COMPA_vect)
 		if(minute > 59)
 		{
 			minute = 0;
-			/*
-			 * first sunday in november decrement hour at 2 am
-			 * second sunday in march increment hour at 2 am
-			 * (twice)
-			 */
+			// FIXME - should be hour == 2, emperical tests
+			// show that it hour == 1 works?
+			//
+			// first Sunday in November decrement hour at 2 am
+			// second Sunday in March increment hour at 2 am
+			// (twice)
+			//
 			if((daylight_time == 0) && (month == 3) && 
 					(hour == 1) && 
 					(day == spring_savings())) {
@@ -465,9 +468,4 @@ ISR(TIMER1_COMPA_vect)
 		}
 	}
 	lcd_display_clock();
-}
-
-ISR(INT0_vect)
-{
-	set_time++;
 }
