ae6af9d9142633acd16ae8c2e335fd8ab29ae686
Chris Gorman <chrisjohgorman@gmail.com>
2018-06-20 13:35:51 -0400

    Removed functions prototypes to display values on lcd and added two to take their place.

--- clock.h
+++ clock.h
@@ -4,24 +4,27 @@
 //
 // function prototypes
 // 
-extern void lcd_display_clock(void);
-extern void lcd_display_second(void);
-extern void lcd_display_minute(void);
-extern void lcd_display_hour(void);
-extern void lcd_display_day(void);
-extern void lcd_display_weekday(void);
-extern void lcd_display_month(void);
-extern void lcd_display_year(void);
-extern void set_second(void);
-extern void set_minute(void);
-extern void set_hour(void);
-extern void set_day(void);
-extern void set_month(void);
-extern void set_year(void);
-extern char spring_savings(void);
-extern char fall_savings(void);
-extern char day_of_week(int, char, char);
-extern char day_of_month(int, char, char, char);
-extern char leap_year(int);
+static void lcd_display_clock(void);
+//static void lcd_display_second(void);
+//static void lcd_display_minute(void);
+//static void lcd_display_hour(void);
+//static void lcd_display_day(void);
+//static void lcd_display_weekday(void);
+//static void lcd_display_month(void);
+//static void lcd_display_year(void);
+static void set_second(void);
+static void set_minute(void);
+static void set_hour(void);
+static void set_day(void);
+static void set_month(void);
+static void set_year(void);
+static char spring_savings(void);
+static char fall_savings(void);
+static char day_of_week(int, char, char);
+static char day_of_month(int, char, char, char);
+static char leap_year(int);
+static inline void lcd_display_date_attribute(uint8_t, uint8_t, uint8_t);
+static inline void lcd_display_date_attribute(uint8_t, uint8_t, uint8_t);
+
 
 #endif // CLOCK_H
