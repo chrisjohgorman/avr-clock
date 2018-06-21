e94d730ac936ae7613391e65b09a007aa26dc54a
Chris Gorman <chrisjohgorman@gmail.com>
2018-06-21 16:21:11 -0400

    Make everything static inline
    - remove unused function names

--- clock.h
+++ clock.h
@@ -4,27 +4,22 @@
 //
 // function prototypes
 // 
-static void lcd_display_clock(void);
-//static void lcd_display_second(void);
-//static void lcd_display_minute(void);
-//static void lcd_display_hour(void);
-//static void lcd_display_day(void);
-//static void lcd_display_weekday(void);
-//static void lcd_display_month(void);
-//static void lcd_display_year(void);
-static void set_second(void);
-static void set_minute(void);
-static void set_hour(void);
-static void set_day(void);
-static void set_month(void);
-static void set_year(void);
-static char spring_savings(void);
-static char fall_savings(void);
-static char day_of_week(int, char, char);
-static char day_of_month(int, char, char, char);
-static char leap_year(int);
-static inline void lcd_display_date_attribute(uint8_t, uint8_t, uint8_t);
-static inline void lcd_display_date_attribute(uint8_t, uint8_t, uint8_t);
-
+static inline void lcd_display_clock(void);
+static inline void lcd_display_day(void);
+static inline void lcd_display_weekday(void);
+static inline void lcd_display_month(void);
+static inline void lcd_display_year(void);
+static inline void set_second(void);
+static inline void set_minute(void);
+static inline void set_hour(void);
+static inline void set_day(void);
+static inline void set_month(void);
+static inline void set_year(void);
+static inline char spring_savings(void);
+static inline char fall_savings(void);
+static inline char day_of_week(int, char, char);
+static inline char day_of_month(int, char, char, char);
+static inline char leap_year(int);
+static inline void lcd_display_time_attribute(uint8_t, uint8_t, uint8_t);
 
 #endif // CLOCK_H
